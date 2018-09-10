7afa778f2b2295be552cc94cae6b4aa6ac36ca69
jiayu <jiayuchen233@gmail.com>
2018-09-10 19:51:53 +0200

    fix small bugs in test data generation module

--- step.sh
+++ step.sh
@@ -13,25 +13,25 @@ test_data_dir=${work_dir}/tests
 python ${script_dir}/create_bam_file.py
 
 # Step 3: Generating genome indexes for STAR
-#mkdir ${test_data_dir}/data/genome
-#STAR --runThreadN 3 --runMode genomeGenerate --genomeDir ${test_data_dir}/data/genome --genomeFastaFiles ${test_data_dir}/data/test1.fa --sjdbGTFfile ${test_data_dir}/data/test1.gtf --sjdbOverhang 14
+mkdir ${test_data_dir}/data/genome || TRUE
+STAR --runThreadN 3 --runMode genomeGenerate --genomeDir ${test_data_dir}/data/genome --genomeFastaFiles ${test_data_dir}/data/test1.fa --sjdbGTFfile ${test_data_dir}/data/test1.gtf --sjdbOverhang 14
 
 
 ## Step 4: Running maping jobs
-#mkdir ${test_data_dir}/data/align
-#STAR --runThreadN 3 --genomeDir ${test_data_dir}/data/genome --readFilesIn ${test_data_dir}/data/test1_1.fq --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ${test_data_dir}/data/align/
+mkdir ${test_data_dir}/data/align || TRUE
+STAR --runThreadN 3 --genomeDir ${test_data_dir}/data/genome --readFilesIn ${test_data_dir}/data/test1_1.fq --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ${test_data_dir}/data/align/
 
 ## Step 5: Using Samtools to create bai file
-#samtools index  ${test_data_dir}/data/align/Aligned.sortedByCoord.out.bam
+samtools index  ${test_data_dir}/data/align/Aligned.sortedByCoord.out.bam
 
 ## Step 6: Generating splicegraph and corresponding count file using spladder
-#rm -rf ${test_data_dir}/data/spladder || TRUE
-#rm ${test_data_dir}/data/test1.gtf.pickle || TRUE
-#python ${spladder_dir}/spladder.py  --insert_ir=n --insert_es=n --insert_ni=n --remove_se=n --validate_sg=n -b ${test_data_dir}/data/align/Aligned.sortedByCoord.out.bam -o ${test_data_dir}/data -a ${test_data_dir}/data/test1.gtf -v y -c 3 -M merge_graphs -T n -P n -p n -q y
+rm -rf ${test_data_dir}/data/spladder || TRUE
+rm ${test_data_dir}/data/test1.gtf.pickle || TRUE
+python ${spladder_dir}/spladder.py  --insert_ir=n --insert_es=n --insert_ni=n --remove_se=n --validate_sg=n -b ${test_data_dir}/data/align/Aligned.sortedByCoord.out.bam -o ${test_data_dir}/data -a ${test_data_dir}/data/test1.gtf -v y -c 3 -M merge_graphs -T n -P n -p n -q y
 
 ## Step 7: remove the existing annotation pickle
-#rm ${test_data_dir}/annotation_preprop.pickle || TRUE
+rm ${test_data_dir}/annotation_preprop.pickle || TRUE
 
 ## Step8: run main file to generate result
-#python ${script_dir}/main_immuno.py --output_dir tests --ann_path ${test_data_dir}/data/test1.gtf --splice_graph ${test_data_dir}/data/spladder/genes_graph_conf3.merge_graphs.pickle --ref_path ${test_data_dir}/data/test1.fa --count_path ${test_data_dir}/data/spladder/genes_graph_conf3.merge_graphs.count.hdf5
+#python ${script_dir}/main_immuno.py --output_dir tests --ann_path ${test_data_dir}/data/test1.gtf --splice_path ${test_data_dir}/data/spladder/genes_graph_conf3.merge_graphs.pickle --ref_path ${test_data_dir}/data/test1.fa --count_path ${test_data_dir}/data/spladder/genes_graph_conf3.merge_graphs.count.hdf5
 
