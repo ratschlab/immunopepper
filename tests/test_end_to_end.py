c1f255920d148796c55dfa9041dfd666088d417f
jiayu <jiayuchen233@gmail.com>
2018-10-26 10:26:47 +0200

    change 'is_filter' to more descriptive argument name 'filter_redundant'

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -53,7 +53,7 @@ def test_end_to_end_ref(test_id, case, mutation_mode, tmpdir):
                '--vcf_path', '{}/test{}{}.vcf'.format(data_dir, test_id, case),
                '--maf_path', '{}/test{}{}.maf'.format(data_dir, test_id, case),
                '--mutation_mode', mutation_mode,
-               '--is_filter']
+               '--filter_redundant']
 
     main_immuno.main(main_immuno.parse_arguments(my_args))
 
