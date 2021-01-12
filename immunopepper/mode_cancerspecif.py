d26effb0e4bcba7a76c46cedd68fb219eb706abe
Laurie Prelot <laurie.prelot@inf.ethz.ch>
2021-01-12 11:31:11 +0100

    MODE cancerspecif: basic cleaning

--- immunopepper/mode_cancerspecif.py
+++ immunopepper/mode_cancerspecif.py
@@ -36,7 +36,6 @@ def DESeq2(count_matrix, design_matrix, normalize, cores=1):
     count_matrix = round(count_matrix)
     count_matrix = pandas2ri.py2rpy(count_matrix)
     design_matrix = pandas2ri.py2rpy(design_matrix)
-    #count_matrix.rownames = count_matrix.get_attrib('kmer')
     design_formula = Formula(' ~ 1')
 
     dds0 = deseq.DESeqDataSetFromMatrix(countData=count_matrix,
@@ -53,7 +52,6 @@ def DESeq2(count_matrix, design_matrix, normalize, cores=1):
                       sfType="poscounts", # Will run 1. estimation of size factors: estimateSizeFactors # parameter "poscounts"
                       fitType="parametric" # 2. estimation of dispersion: estimateDispersions # parameter "parametric"
                       )
-    #normalized_count_matrix = deseq.counts(self, normalized=Tru os.path.join(cancer_dir, 'expression_counts.libsize.tsv'),e)
 
     deseq_result = deseq.results(dds)
     fit_res = to_dataframe(deseq_result)
@@ -105,13 +103,11 @@ def mode_cancerspecif(arg):
         def I_L_replace(value):
             return value.replace('I', 'L')
 
-        #spark.sparkContext.addFile('/Users/laurieprelot/software/anaconda/lib/python3.6/sitesddpackages/scipy.zip') # TODO do we need it ? packages in config
 
         ### Preprocessing Normals
         logging.info(">>>>>>>> Preprocessing Normal samples")
         index_name = 'kmer'
         jct_col = "iscrossjunction"
-
 
         # Rename
         rename = False #For development 
@@ -123,21 +119,11 @@ def mode_cancerspecif(arg):
         else: 
             normal_matrix = spark.read.parquet(arg.path_normal_matrix_segm)
 
-
-        #logging.info("repartition")
-        #normal_matrix =  normal_matrix.repartition(arg.cores * 10)
-        #logging.info("partitions: {}".format(normal_matrix.rdd.getNumPartitions()))
-
+        # Drop junction column
         normal_matrix = normal_matrix.drop(jct_col)
-        
-        #logging.info("cache")
-        #normal_matrix.cache()
-        #logging.info("checkpoint")
-        #normal_matrix.checkpoint()
 
-        # Cast type and fill nans
+        # Cast type and fill nans + Reduce samples (columns) to whitelist
         logging.info("Cast types")
-
         if arg.whitelist is not None:
             whitelist = pd.read_csv(arg.whitelist, sep = '\t', header = None)[0].to_list()
             whitelist = [name_.replace('-', '').replace('.', '').replace('_', '') for name_ in whitelist]
@@ -146,46 +132,25 @@ def mode_cancerspecif(arg):
         else:
             normal_matrix =  cast_type_dbl(normal_matrix, normal_matrix.schema.names, index_name)
 
-
+        # Fill Nans
         logging.info("Remove Nans")
         normal_matrix = normal_matrix.na.fill(0)
 
+        # Remove kmers abscent from all samples
         logging.info("Remove non expressed kmers SQL-")
-
-        #logging.info("cache")
-        #normal_matrix.cache()
-        #logging.info("checkpoint")
-        #normal_matrix.checkpoint()
         logging.info("partitions: {}".format(normal_matrix.rdd.getNumPartitions()))
-
-
-        # Remove lines of Nans (Only present in junction file)
-        # normal_matrix = normal_matrix.withColumn('allnull', sum(normal_matrix[name_] for name_ in normal_matrix.schema.names if name_ != index_name))
-        # normal_matrix = normal_matrix.filter(sf.col("allnull") > 0.0 )
-        # normal_matrix = normal_matrix.drop("allnull")
-
         not_null = ' OR '.join(
             ['({} != 0.0)'.format(col_name)
         for col_name in normal_matrix.schema.names if col_name != index_name])  # SQL style  # All zeros
         normal_matrix = normal_matrix.filter(not_null)
 
-        #logging.info("cache")
-        #normal_matrix.cache()
-        #logging.info("checkpoint")
-        #normal_matrix.checkpoint()
-        logging.info("partitions: {}".format(normal_matrix.rdd.getNumPartitions()))
-
-        logging.info("Make unique")
         # Make unique
-
+        logging.info("Make unique")
+        logging.info("partitions: {}".format(normal_matrix.rdd.getNumPartitions()))
         exprs = [sf.max(sf.col(name_)).alias(name_) for name_ in  normal_matrix.schema.names if name_ != index_name]
         normal_matrix = normal_matrix.groupBy(index_name).agg(*exprs)
-
-        #logging.info("cache")
-        #normal_matrix.cache()
-        #logging.info("checkpoint")
-        #normal_matrix.checkpoint()
         logging.info("partitions: {}".format(normal_matrix.rdd.getNumPartitions()))
+
 
         ### Preprocessing Libsize
         logging.info(">>>>>>>> Preprocessing libsizes")
@@ -220,8 +185,6 @@ def mode_cancerspecif(arg):
                 high_expr_normals = normal_matrix.filter(highly_expressed_normals).select(sf.col(index_name))
                 normal_matrix = normal_matrix.filter(ambigous_expression_normals)  # TODO add condition empty matrix
 
-                #normal_matrix.cache()
-                #normal_matrix.checkpoint()
 
             if arg.tissue_grp_files is not None:
                 modelling_grps = []
@@ -258,8 +221,8 @@ def mode_cancerspecif(arg):
         ### NORMALS: Hard Filtering
         else:
             logging.info(">>>>>>>> Hard Filtering")
-        # Filter based on expression level in at least n samples
 
+        ### Keep normal kmers with expression level in at least n samples
             logging.info("expression filter")
 
             if libsize_n is not None:
@@ -271,58 +234,41 @@ def mode_cancerspecif(arg):
                     sf.when(sf.col(name_)  > arg.expr_limit_normal, 1).otherwise(0).alias(name_)
                     for name_ in normal_matrix.schema.names if name_ != index_name])
             
-            #logging.info("cache")
-            #normal_matrix.cache()
-            #logging.info("checkpoint")
-            #normal_matrix.checkpoint()
-            logging.info("partitions: {}".format(normal_matrix.rdd.getNumPartitions()))    
-            
+            logging.info("partitions: {}".format(normal_matrix.rdd.getNumPartitions()))
             logging.info("reduce")
             rowsum = (reduce(add, (sf.col(x) for x in normal_matrix.columns[1:]))).alias("sum")
-
-            #logging.info("cache")
-            #normal_matrix.cache()
-            #logging.info("checkpoint")
-            #normal_matrix.checkpoint()
             logging.info("partitions: {}".format(normal_matrix.rdd.getNumPartitions()))
-            
             logging.info("filter")
             normal_matrix = normal_matrix.filter(rowsum >= arg.expr_n_limit)
-
-            # for name_ in normal_matrix.schema.names:
-            #     if name_ != index_name:
-            #         normal_matrix = normal_matrix.withColumn(name_, sf.when(
-            #             sf.col(name_) / libsize_n.loc[name_, "libsize_75percent"] > arg.expr_limit_normal, 1).otherwise(
-            #             0))  # Normalize on the fly
-            # normal_matrix = normal_matrix.withColumn('passing_expr_criteria', sum(
-            #     normal_matrix[name_] for name_ in normal_matrix.schema.names if name_ != index_name))
-            # normal_matrix = normal_matrix.filter(sf.col("passing_expr_criteria") >= arg.expr_n_limit)
-
-        #normal_matrix.cache()
-        #normal_matrix.checkpoint()
-
         logging.info("No caching")
+
+
         ### Apply filtering to foreground
+
         if arg.expression_fields_c is None:
             expression_fields_orig =  ['segment_expr', 'junction_expr']
         else:
             expression_fields_orig = arg.expression_fields_c
+
         expression_fields = [name_.replace('-', '').replace('.', '').replace('_', '') for name_ in expression_fields_orig]
         drop_cols = ['id']
         for cancer_path, cancer_sample in zip(arg.paths_cancer_samples, arg.ids_cancer_samples):
             cancer_sample = cancer_sample.replace('-', '').replace('.', '').replace('_', '')
+
+            # Preprocess cancer samples
             logging.info("... Process cancer sample {}".format(cancer_sample))
             cancer_path_tmp = pq_WithRenamedCols(cancer_path)
             cancer_kmers = spark.read.parquet(cancer_path_tmp)
             logging.info("Renamed done")
+            # Drop junction colum
             for drop_col in drop_cols:
                 cancer_kmers = cancer_kmers.drop(sf.col(drop_col))
             logging.info("Collapse kmer horizontal")
-            # Remove the '/' in the data
+            # Remove the '/' in the expression data (kmers duplicate within a gene have 'expression1/expression2' format
             local_max = sf.udf(collapse_values, st.FloatType())
             for name_ in expression_fields:
                 cancer_kmers = cancer_kmers.withColumn(name_, local_max(name_))
-
+            # Filter on juction status
             if arg.cross_junction == 1:
                 logging.info("Keep cross junction kmers")
                 cancer_kmers = cancer_kmers.filter("{} == True".format(jct_col))
@@ -337,27 +283,22 @@ def mode_cancerspecif(arg):
             if len(cancer_kmers.head(1)) == 0:
                 logging.info("WARNING: Restriction on junction status removed all foreground... Exiting")
                 sys.exit(1)
-
-
+            # Make kmers unique (Take max expression)
             logging.info("Collapse kmer vertical")
-            # Make unique, max
             cancer_kmers = cancer_kmers.withColumn(jct_col, sf.col(jct_col).cast("boolean").cast("int"))
             exprs = [sf.max(sf.col(name_)).alias(name_) for name_ in cancer_kmers.schema.names if name_ != index_name]
             cancer_kmers = cancer_kmers.groupBy(index_name).agg(*exprs)
-            #cancer_kmers = cancer_kmers.sort(sf.col(index_name))
-
-            #Remove double zeros filelds
+            # Remove kmers unexpressed (both junction and segment expression null)
             cancer_kmers = cancer_kmers.withColumn('allnull', sum(cancer_kmers[name_] for name_ in expression_fields))
             cancer_kmers = cancer_kmers.filter(sf.col("allnull") > 0.0)
             cancer_kmers = cancer_kmers.drop("allnull")
-
             #Normalize by library size
             if libsize_c is not None:
                 cancer_kmers = cancer_kmers.withColumn(name_, sf.round(cancer_kmers[name_] /libsize_c.loc[cancer_sample, "libsize_75percent"], 2))
             else:
                 cancer_kmers = cancer_kmers.withColumn(name_, sf.round( cancer_kmers[name_] , 2))
 
-            #Perform Background removal
+            ###Perform Background removal
             logging.info("Perform join")
             logging.info("NO BROADCAST")
             #normal_matrix = sf.broadcast(normal_matrix)
@@ -375,7 +316,7 @@ def mode_cancerspecif(arg):
                 logging.info("WARNING: normal filtering removed all Foreground")
 
 
-            # Remove Uniprot
+            ### Remove Uniprot
             if arg.uniprot is not None:
                 uniprot = spark.read.csv(arg.uniprot, sep='\t', header=None)
                 uniprot_header = index_name + "_IL_eq"
@@ -397,6 +338,10 @@ def mode_cancerspecif(arg):
                 logging.info("WARNING: uniprot filtering removed all Foreground")
 
             os.remove(cancer_path_tmp)
+
+
+
+
             #TODO Implement the intersection of the modelling tissues
             #TODO transfer to junction xpression
 
