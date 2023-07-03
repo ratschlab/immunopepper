097097fded4bafef8e7c0511fe265e39b86ecbd6
pilarbc123 <100405572@alumnos.uc3m.es>
2023-07-03 17:02:54 +0200

    Fix loggers and filtering message

--- immunopepper/mode_mhcbind.py
+++ immunopepper/mode_mhcbind.py
@@ -1,13 +1,26 @@
 import glob
 import logging
 import os
-import pandas as pd 
+import pandas as pd
 import shlex
-import sys 
+import sys
 
 from mhctools.cli.script import main as mhctools_main
 
 def mode_mhcbind(arg):
+    # Configure the logger
+    if arg.verbose == 0:
+        log_level = logging.WARNING
+    elif arg.verbose == 1:
+        log_level = logging.INFO
+    else:
+        log_level = logging.DEBUG
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(logging.Formatter("%(asctime)-15s %(levelname)-8s %(message)s"))
+    stdout_handler.setLevel(log_level)
+    logging.getLogger().addHandler(stdout_handler)
+
     args_list=shlex.split(arg.argstring)
     if '--output-csv' not in args_list:
         logging.error('output-csv was not provided in --arg.argstring')
@@ -24,26 +37,27 @@ def mode_mhcbind(arg):
              sys.exit(1)
         input_= pd.concat(map(lambda file: pd.read_csv(file , sep ='\t'), input_list))
         input_[['kmer']].to_csv(input_peptides_file, header = None, index = None)
-    
+
     ### Launch MHC Tools
     logging.info("Launch MHC Tools with command {}".format(arg.argstring))
     os.environ["PATH"] +=":{}".format(arg.mhc_software_path)
     mhctools_main(args_list)
-    
+
     output_csv_idx = [i+1 for i,j in enumerate(args_list) if j == '--output-csv']
     output_csv_path = args_list[output_csv_idx[0]]
     output_csv = pd.read_csv(output_csv_path, sep = ',' )
     ### Merge the outputs from MHC bind and cancerspecif modes
     if '--input-peptides-file' in args_list and arg.partitioned_tsv:
         output_csv = input_.merge( output_csv, left_on ='kmer', right_on = 'peptide').drop('peptide', axis = 1)
-        
+
     ### Perform some rank or affinity binding filtering
     if (arg.bind_score_method is not None) and (arg.bind_score_threshold is not None):
-        logging.info("Perform filtering with {} >= {}".format(arg.bind_score_method, arg.bind_score_threshold))
         if arg.less_than:
+            logging.info("Perform filtering with {} <= {}".format(arg.bind_score_method, arg.bind_score_threshold))
             output_csv = output_csv.loc[output_csv[arg.bind_score_method] <= arg.bind_score_threshold, :]
             saving_suffix='_With{}LessLim{}'.format(arg.bind_score_method, arg.bind_score_threshold)
         else:
+            logging.info("Perform filtering with {} >= {}".format(arg.bind_score_method, arg.bind_score_threshold))
             output_csv = output_csv.loc[output_csv[arg.bind_score_method] >= arg.bind_score_threshold, :]
             saving_suffix='_With{}MoreLim{}'.format(arg.bind_score_method, arg.bind_score_threshold)
     else:
@@ -51,9 +65,9 @@ def mode_mhcbind(arg):
 
     if 'tsv' or 'csv' in output_csv:
         base_out = '.'.join(output_csv_path.split('.')[:-1])
-    else: 
+    else:
         base_out = output_csv_path
     format_tag = '.tsv'
-    
-    logging.info("Saving to {}".format(base_out + saving_suffix + format_tag)) 
+
+    logging.info("Saving to {}".format(base_out + saving_suffix + format_tag))
     output_csv.to_csv( base_out + saving_suffix + format_tag, sep = '\t', index = None)
